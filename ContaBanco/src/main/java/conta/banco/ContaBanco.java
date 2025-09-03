ad7ebfa25f7e718fdc169fa2d76e81fe139f45dc
leonardo <leo.srodrigues01@gmail.com>
2025-09-03 13:16:27 -0300

    criar projeto

---
+++ ContaBanco/src/main/java/conta/banco/ContaBanco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package conta.banco;
+
+/**
+ *
+ * @author leo
+ */
+public class ContaBanco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
